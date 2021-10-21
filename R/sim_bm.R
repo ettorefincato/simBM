d42c05736ff00ba596ad51018f17afcff2d4513c
ettore.fincato <zc21901@bristol.ac.uk>
2021-10-21 15:16:58 +0100

    fpt added

--- R/sim_bm.R
+++ R/sim_bm.R
@@ -11,14 +11,59 @@
 #'
 #' @examples
 
-simulate <- function(sigma_2, nsim, dt, init_val){
+simulate <- function(nsim, sigma_2, init_val){
 
-  B <- c()
-  B[1] <- init_val # BM starts from init_val
+  bm <- c()
+  bm[1] <- init_val # BM starts from init_val
   for (i in 2:nsim) {
     innov <- rnorm(1,0, sigma_2) # generate one innovation
-    B[i] <- B[i-1] + innov # generate next value of BM
+    bm[i] <- bm[i-1] + innov # generate next value of BM
   }
-  return(B)
+
+
+  return(bm)
 
 }
+
+
+
+
+#approximate bm with linear interpolation, then use naive Montecarlo generating
+# dens*length(bm) time values and evaluate the function on those values. Then, take
+#the minimum time for which the function is bigger than the treshold a
+
+#' Title
+#'
+#' @param bm
+#' @param a
+#' @param dens
+#' @param ...
+#'
+#' @return
+#' @export firstpassage
+#'
+#' @examples
+
+firstpassage <- function(bm,a, dens){
+
+  if(a<0){
+    return("a must be positive")
+  }
+
+  fun <- approxfun(x=seq(1,length(bm),1), y = bm, method = "linear")
+  u<- runif(dens*length(bm),0,length(bm))
+  fpt <- min(na.omit(u[fun(u)>a]))
+
+  if(fpt==Inf){
+    return("level a never passed")
+  }
+
+
+  return(fpt)
+
+  }
+
+
+
+
+
